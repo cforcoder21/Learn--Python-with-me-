7bb9cac460ec73e5bf019058153ed22f3f6821b7
Ayush Sinha <AYUSH.23FE10CSE00073@MUJ.MANIPAL.EDU>
2024-08-25 06:05:38 +0000

    updated

---
+++ python_basics/# %% [markdown].py
@@ -0,0 +1,18 @@
+# %% [markdown]
+# Creating a python program that stores a value in a variable;
+# 
+
+# %%
+a= 21
+print(21)
+
+# %%
+number01= 21
+number02= 22
+number03= 23
+print("Numbers : ",number01," ", number02," ", number03)
+
+# %%
+
+
+
